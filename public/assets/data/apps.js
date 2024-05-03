7cd2d35dc518750863cbc77bd715b14d7e7b64d3
zoobuh <167261166+zoobuh@users.noreply.github.com>
2024-05-03 00:48:01 -0400

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -79,7 +79,7 @@ var apps = [
     },
     {
       'id': 'watchwave',
-      'title': 'Watch Wave',
+      'title': 'WatchWave',
       'url': 'https://foxohub1.vercel.app/service/hvtrs8%2F-wctahuate%2Fv0.tepcgl%2Carp-lcnfilg',
       "image": "hyg"
        "description": "Free Movie Streaming Site"
